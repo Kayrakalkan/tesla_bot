86e9488eb074492174e2d6a615e166486e7fbc64
Kayrakalkan <kalkankayra@hotmail.com>
2025-05-04 19:01:47 +0300

    updated2

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def send_sms(message):
 
 async def check_stock():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
 
         await page.goto('https://www.tesla.com/tr_TR/modely/design#overview')
